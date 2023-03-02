cf9446ea2f5811b80fb9e0efde3d2bc17ab75c6e
Grigorii Skovorodnikov <gskovorod3@gmail.com>
2023-03-02 21:41:41 +0500

    add dz 17

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+int[] InputArray(string text)
+{
+    System.Console.WriteLine(text);
+    return  Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void Positive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine(count);
+}
+
+int[] myArray = InputArray("Input array ");
+PrintArray(myArray);
+Positive(myArray);
